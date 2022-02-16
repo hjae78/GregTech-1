990066621018550c2cfa4e41518c84ad08e4bb0c
Rongmario <rongmario@protonmail.com>
2022-02-16 23:06:06 +0000

    Relocate tool jsons + register oredict + optimize ice harvest logic

--- src/main/java/gregtech/common/CommonProxy.java
+++ src/main/java/gregtech/common/CommonProxy.java
@@ -6,7 +6,7 @@ import gregtech.api.block.machines.MachineItemBlock;
 import gregtech.api.enchants.EnchantmentEnderDamage;
 import gregtech.api.enchants.EnchantmentHardHammer;
 import gregtech.api.items.metaitem.MetaItem;
-import gregtech.api.items.toolitem.GTToolDefinition;
+import gregtech.api.items.toolitem.IGTTool;
 import gregtech.api.recipes.ModHandler;
 import gregtech.api.recipes.crafttweaker.MetaItemBracketHandler;
 import gregtech.api.recipes.recipeproperties.FusionEUToStartProperty;
@@ -133,7 +133,7 @@ public class CommonProxy {
             item.registerSubItems();
         }
 
-        for (GTToolDefinition tool : ToolItems.getAllTools()) {
+        for (IGTTool tool : ToolItems.getAllTools()) {
             registry.register(tool.get());
         }
 
@@ -214,6 +214,7 @@ public class CommonProxy {
         GTLog.logger.info("Registering ore dictionary...");
 
         MetaItems.registerOreDict();
+        ToolItems.registerOreDict();
         MetaBlocks.registerOreDict();
         OreDictionaryLoader.init();
         MaterialInfoLoader.init();
